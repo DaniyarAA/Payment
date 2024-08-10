61560f56046f848fd9b381a73d0b9499a8c14391
DaniyarM. <gitmailhub@gmail.com>
2024-08-10 14:55:31 +0600

    added validation in UserDto

--- src/main/java/kg/attractor/payment/dto/UserDto.java
+++ src/main/java/kg/attractor/payment/dto/UserDto.java
@@ -1,5 +1,9 @@
 package kg.attractor.payment.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +15,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserDto {
     private Long id;
+    @NotBlank(message = "Phone number cannot be blank")
     private String phoneNumber;
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+    @NotBlank
+    @Size(min = 4, max = 20,
+            message = "length must be >= 4 and <= 20")
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).+$",
+            message = "should contain at least one uppercase letter, one number")
     private String password;
 }
